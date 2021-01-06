f674caf83a34e0a5ea730b718a3648deec30438d
Chezzyz <48602862+Chezzyz@users.noreply.github.com>
2021-01-06 23:24:19 +0500

    Кнопочки чето еще

--- MagicCards/Assets/Scripts/Training.cs
+++ MagicCards/Assets/Scripts/Training.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Training : MonoBehaviour
 {
     [SerializeField] Text stepsCount;
-    [SerializeField] Text levelText;
+    [SerializeField] TextMeshProUGUI levelText;
     [SerializeField] Text helperText;
     public int steps = 0;
 
